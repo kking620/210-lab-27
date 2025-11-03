8dba50e36f17b8156f6dee2bf4a8126af6463622
Krispy King <kking620@gmail.com>
2025-11-02 17:32:01 -0800

    Third Commit

--- 210-demo-std-map.cpp
+++ 210-demo-std-map.cpp
@@ -8,17 +8,18 @@ using namespace std;
 
 int main() {
     // declarations
-    map<string, tuple<int, string, string>> villagerDetails;
+    tuple<int, string, string> villagerDetails;
+    map<string, villagerDetails> villagers;
 
     // insert elements into the map
     // note how the right-hand side of the assignment are the vector elements
-    villagerDetails["Audie"] = {5, "Human", "Howdy partner!"};
-    villagerDetails["Raymond"] = {3, "Werewolf", "I'm not a wolf, I promise."};
-    villagerDetails.insert({"Marshal", {10, "Human", "This place is so cool!"}});
+    villagers["Audie"] = {5, "Human", "Howdy partner!"};
+    villagers["Raymond"] = {3, "Werewolf", "I'm not a wolf, I promise."};
+    villagers.insert({"Marshal", {10, "Human", "This place is so cool!"}});
 
     // access the map using a range-based for loop
     cout << "Villagers and their favorite colors (range-based for loop):" << endl;
-    for (auto pair : villagerDetails) {
+    for (auto pair : villagers) {
         cout << pair.first << ": ";
         for (auto color : pair.second)
             cout << color << " ";
@@ -27,8 +28,8 @@ int main() {
 
     // access the map using iterators
     cout << "\nVillagers and their favorite colors (iterators):" << endl;
-    for (map<string, vector<string>>::iterator it = villagerDetails.begin(); 
-                                               it != villagerDetails.end(); ++it) {
+    for (map<string, vector<string>>::iterator it = villagers.begin(); 
+                                               it != villagers.end(); ++it) {
         cout << it->first << ": ";
         for (auto color : it->second) {
             cout << color << " ";
@@ -37,12 +38,12 @@ int main() {
     }
 
     // delete an element
-    villagerDetails.erase("Raymond");
+    villagers.erase("Raymond");
 
     // search for an element using .find() to avoid errors
     string searchKey = "Audie";
-    auto it = villagerDetails.find(searchKey);
-    if (it != villagerDetails.end()) {  // the iterator points to beyond the end of the map
+    auto it = villagers.find(searchKey);
+    if (it != villagers.end()) {  // the iterator points to beyond the end of the map
                                        // if searchKey is not found
         cout << "\nFound " << searchKey << "'s favorite colors: ";
         for (auto color : it->second)  // range loop to traverse the value/vector
@@ -52,9 +53,9 @@ int main() {
         cout << endl << searchKey << " not found." << endl;
 
     // report size, clear, report size again to confirm map operations
-    cout << "\nSize before clear: " << villagerDetails.size() << endl;
-    villagerDetails.clear();
-    cout << "Size after clear: " << villagerDetails.size() << endl;
+    cout << "\nSize before clear: " << villagers.size() << endl;
+    villagers.clear();
+    cout << "Size after clear: " << villagers.size() << endl;
 
     return 0;
 }
